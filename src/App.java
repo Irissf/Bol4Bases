1c468eed458cc048afa5d183d187ee795993e944
Iris SF <iris.kosy@gmail.com>
2021-01-20 17:00:56 +0100

    cambios

--- src/App.java
+++ src/App.java
@@ -1,21 +1,16 @@
 import java.sql.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         Conexion conexion = new Conexion();
         conexion.abrirConexion("add", "localhost", "root", "");
-
-        Ejercicios ejer = new Ejercicios();
-
-        //Ejercicio 1 =========================================
         try {
-            ejer.ConsultaAlumno("La", conexion);
+            //conexion.consultaAlumno("la");
+            conexion.insertarAlumno("Marta", "Compi", 165, 31);
         } catch (SQLException e) {
-            System.out.println("Error de base de datos");
+            System.out.println("error: "+e.getMessage());
         }
-        
-
         conexion.cerrarConexion();
         
     }
--- src/Conexion.java
+++ src/Conexion.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,8 +12,10 @@ public class Conexion {
         try {
             String url = String.format("jdbc:mariadb://%s:3306/%s", servidor, bd);
             this.conexion = DriverManager.getConnection(url, usuario, password);// Establecemos la conexión con la BD
-            if (this.conexion != null)
+            if (this.conexion != null){
                 System.out.println("Conectado a la base de datos " + bd + " en " + servidor);
+            }
+                
             else
                 System.out.println("No se ha conectado a la base de datos " + bd + " en " + servidor);
         } catch (SQLException e) {
@@ -33,4 +36,81 @@ public class Conexion {
             System.out.println("Error al cerrar la conexión: " + e.getLocalizedMessage());
         }
     }
+
+    //========================================EJERCICIO 1=====================================================================
+    /**
+     * Un método que permita consultar alumnos que contengan una cadena de caracteres en su nombre.
+        Además deberá visualizar el número de resultados obtenidos.
+     * @param cadena
+     * @throws SQLException
+     */
+
+    public void consultaAlumno(String cadena) throws SQLException {
+
+        String consulta = "SELECT * FROM alumnos WHERE nombre LIKE \"%"+cadena+"%\""; // Consulta a ejecutar
+
+        Statement stmt = conexion.createStatement();
+        ResultSet rs = stmt.executeQuery(consulta); // Se ejecuta la consulta
+        while (rs.next()) { // Mientras queden filas en rs (el método next devuelve true) recorremos las
+                            // filas
+            System.out.println(rs.getInt(1) + "\t" + // Se obtiene datos en función del número de columna
+                    rs.getString("nombre") + "\t" + rs.getString("apellidos")); // o de su nombre
+        }
+        stmt.close(); // Se cierra el Statement
+        cerrarConexion();
+    }
+
+    //NOTAS executeQuery para consultas que recibes datos, executeUpdate consultas que no reciben datos pero si los
+    
+    //========================================EJERCICIO 2=====================================================================
+    /**
+     *  Dar de alta alumnos y asignaturas
+     * //INSERT INTO alumnos(nombre, apellidos, altura, aula) VALUES ('Carlos', 'Seijo', 180, 99); 
+     * @param nombre
+     * @param apellido
+     * @param altura
+     * @param aula
+     */
+
+    public void insertarAlumno(String nombre, String apellido, int altura, int aula) throws SQLException{
+
+        String consulta = "INSERT INTO alumnos(nombre, apellidos, altura, aula) VALUES ('" +nombre+ "', '"+ apellido +"',"+altura+", "+ aula +")";
+
+        boolean numAulaCorrecto = comprobarInt("numero", "aulas", aula);
+        Statement statement = conexion.createStatement();
+         
+        if(numAulaCorrecto){
+            statement.executeUpdate(consulta);
+            System.out.println("los datos fueron introducidos correctamente");
+        }else{
+            System.out.println("No existe esa aula");
+        }
+    }
+
+    public void insertarAsignatura(int cod, String nombre){
+
+        String consulta = "SELECT cod FROM asignaturas";
+    }   
+
+
+    public boolean comprobarInt(String campo, String tabla, int numComparar){
+        String consulta = "SELECT " + campo + " FROM " + tabla + "";
+
+        try {
+            Statement statement = conexion.createStatement();
+            ResultSet result = statement.executeQuery(consulta);
+
+            while (result.next()) {
+                if(result.getInt(campo) == numComparar){
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error:" +e.getMessage());
+        }
+
+        return false;
+        
+    }
+
 }
